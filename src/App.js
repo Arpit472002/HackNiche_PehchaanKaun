77b9f39e53f0ba672a4c46596bd68573a79106bb
Bhavik Shah <80097065+bhavikshah2002@users.noreply.github.com>
2023-02-17 10:28:00 +0530

    Frontend in Main

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/login";
+import AuthProvider from "./context/userContext";
+import Signup from "./components/signup";
+
+function App() {
+  return (
+    <>
+    <div>
+      <AuthProvider>
+      <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+        <Router>
+          <Routes>
+            <Route exact path="/login" element={<Login />}></Route>
+            <Route exact path="/signup" element={<Signup />}></Route>
+          </Routes>
+        </Router>
+      </AuthProvider>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/context/userContext.js
@@ -0,0 +1,117 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+export const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const refresh = async () => {
+    const userId = localStorage.getItem("id");
+    if (!userId) {
+      setUser(null);
+      return;
+    }
+
+    try {
+      const { data } = await axios.get(`${process.env.REACT_APP_PUBLIC_URL}/accounts/MyUser/` + userId + "/");
+      setUser({
+        userName: data.username,
+        firstName: data.first_name,
+        lastName: data.last_name,
+        email: data.email,
+        dob: data.dob,
+        bio: data.bio,
+        photoUrl: process.env.NEXT_PUBLIC_API + data.profile_pic,
+        userId: data.user_id,
+      });
+    } catch (e) {
+      setUser(null);
+    }
+  };
+
+  const login = async ({ email, password }) => {
+    try {
+      console.log(process.env.REACT_APP_PUBLIC_URL);
+      localStorage.removeItem('token')
+      var o =  {email: email,password: password }
+      const res = await axios.post(`${process.env.REACT_APP_PUBLIC_URL}/accounts/login/`,o);
+      setUser(res.data);
+
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("id", res.data.user_id);
+
+      toast.success("Login Successfull");
+
+      refresh();
+
+      return { ...res.data, success: true };
+    } catch (error) {
+      toast.error("Wrong Credentials");
+      return { ...error, success: false };
+    }
+  };
+
+  const register = async ({
+    email,
+    password,
+    firstName,
+    lastName,
+    userName,
+  }) => {
+    try {
+      localStorage.removeItem('token')
+      const res = await axios.post(`${process.env.REACT_APP_PUBLIC_URL}/accounts/register/`, {
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        username: userName,
+        password: password,
+      });
+
+      // @ Sucessful
+      if (res.data.username === userName) {
+        setUser(res.data);
+        toast.success("Verification Mail Sent.");
+        return { ...res.data, success: true };
+      }
+      // @ Email exists aready
+      else if (res.data.email) {
+        toast.error("Email Already Registerd.");
+        return { ...res.data, success: false };
+      } else {
+        toast.error(res.data.username);
+        return { ...res.data, success: false };
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+      return { ...error, success: false };
+    }
+  };
+
+  const logout = async () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("id");
+
+    setUser(null);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    refresh();
+    setLoading(false);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, setUser, loading, error, register, login, logout }}
+    >
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useUser = () => useContext(AuthContext);
