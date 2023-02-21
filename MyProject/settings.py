18ac32ecc8892728b82a479c8f01065a4a4374f2
Arpit472002 <sarpit623@gmail.com>
2023-02-21 20:23:42 +0530

    something

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -138,13 +138,13 @@ REST_FRAMEWORK = {
     ]
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER=config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
-FRONT_END_HOST="http://localhost:3000"
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+# FRONT_END_HOST="http://localhost:3000"
 
 AUTH_USER_MODEL = 'accounts.MyUser'
 
